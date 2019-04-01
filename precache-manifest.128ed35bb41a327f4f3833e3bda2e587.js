f867ee53ca0db01b0169ab72486a65a20867fccf
Marivaldo Sena <msena.ifsp@gmail.com>
2019-04-01 08:42:29 -0300

    Updates

---
+++ precache-manifest.128ed35bb41a327f4f3833e3bda2e587.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd74047815334976b9af",
+    "url": "/gerdau-todos-frontend/static/js/runtime~main.d304e254.js"
+  },
+  {
+    "revision": "7cc8f7539eecdb06ada7",
+    "url": "/gerdau-todos-frontend/static/js/main.eaff040f.chunk.js"
+  },
+  {
+    "revision": "c092d627d6a1772aa137",
+    "url": "/gerdau-todos-frontend/static/js/2.1833d11f.chunk.js"
+  },
+  {
+    "revision": "7cc8f7539eecdb06ada7",
+    "url": "/gerdau-todos-frontend/static/css/main.c4d40017.chunk.css"
+  },
+  {
+    "revision": "7527ded4eed5f413d9586bbefc499276",
+    "url": "/gerdau-todos-frontend/index.html"
+  }
+];
